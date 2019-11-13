03af5c7477dd639c54c394c44543c127bb469b5e
NicPav1 <52611402+NicPav1@users.noreply.github.com>
2019-11-12 20:14:38 -0600

    Add files via upload

---
+++ mapreduce.c
@@ -0,0 +1,20 @@
+#include "mapreduce.h"
+
+void MR_Emit(char *key, char *value) {
+    
+}
+
+unsigned long MR_DefaultHashPartition(char *key, int num_partitions) {
+
+}
+
+unsigned long MR_SortedPartition(char *key, int num_partitions) {
+
+}
+
+void MR_Run(int argc, char *argv[], 
+            Mapper map, int num_mappers, 
+            Reducer reduce, int num_reducers, 
+            Partitioner partition, int num_partitions) {
+
+}
