8adfaa455cc9b8a58ff120a204ed78144d0d0192
NicPav1 <52611402+NicPav1@users.noreply.github.com>
2019-11-14 10:37:33 -0600

    Started Project

--- mapreduce.c
+++ mapreduce.c
@@ -1,11 +1,22 @@
 #include "mapreduce.h"
 
+// Used to store key-value pairs?
+typedef struct 
+{
+    char key;
+    char value;
+} key_value;
+
 void MR_Emit(char *key, char *value) {
     
 }
 
 unsigned long MR_DefaultHashPartition(char *key, int num_partitions) {
-
+    unsigned long hash = 5381;
+    int c;
+    while ((c = *key++) != '\0')
+        hash = hash * 33 + c;
+    return hash % num_partitions;
 }
 
 unsigned long MR_SortedPartition(char *key, int num_partitions) {
